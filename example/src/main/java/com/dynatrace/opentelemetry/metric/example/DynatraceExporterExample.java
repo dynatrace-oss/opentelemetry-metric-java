0b9e65344c56fcbc64ab6020ba78f98365846296
Armin Ruech <armin.ruech@dynatrace.com>
2020-12-02 15:23:25 +0100

    Add example

---
+++ example/src/main/java/com/dynatrace/opentelemetry/metric/example/DynatraceExporterExample.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2020 Dynatrace LLC
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dynatrace.opentelemetry.metric.example;
+
+import com.dynatrace.opentelemetry.metric.DynatraceMetricExporter;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Labels;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.export.IntervalMetricReader;
+import java.util.Collections;
+
+public class DynatraceExporterExample {
+  public static void main(String[] args) throws Exception {
+    DynatraceMetricExporter exporter;
+    if (args.length == 2) {
+      // Endpoint URL and API token passed as args
+      String endpointUrl = args[0];
+      String apiToken = args[1];
+      System.out.println("Setting up DynatraceMetricExporter to export to " + endpointUrl);
+      exporter =
+          DynatraceMetricExporter.builder().setUrl(endpointUrl).setApiToken(apiToken).build();
+    } else {
+      // default is to export to local OneAgent
+      System.out.println("No endpoint URL and API token passed as command line args");
+      System.out.println("Setting up DynatraceMetricExporter to export to local OneAgent endpoint");
+      exporter = DynatraceMetricExporter.getDefault();
+    }
+
+    IntervalMetricReader intervalMetricReader =
+        IntervalMetricReader.builder()
+            .setMetricProducers(
+                Collections.singleton(
+                    OpenTelemetrySdk.getGlobalMeterProvider().getMetricProducer()))
+            .setExportIntervalMillis(5000)
+            .setMetricExporter(exporter)
+            .build();
+
+    // Gets or creates a named meter instance
+    Meter meter =
+        OpenTelemetry.getGlobalMeter("com.dynatrace.opentelemetry.metric.example", "0.1.0-beta");
+
+    // Create a counter
+    LongCounter counter =
+        meter
+            .longCounterBuilder("example_counter")
+            .setDescription("Just some counter used as an example")
+            .setUnit("1")
+            .build();
+
+    // Use a bound counter with a pre-defined label set
+    LongCounter.BoundLongCounter someWorkCounter =
+        counter.bind(Labels.of("some_dimension", "dimension1"));
+
+    while (true) {
+      // Record data with bound labels
+      someWorkCounter.add(1);
+
+      // Or record data on unbound counter and explicitly specify the label set at call-time
+      counter.add(2, Labels.of("some_dimension", "dimension2"));
+
+      Thread.sleep(1000);
+    }
+  }
+}
