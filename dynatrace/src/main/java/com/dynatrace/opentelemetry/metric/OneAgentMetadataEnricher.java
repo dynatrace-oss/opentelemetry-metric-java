ba2e6f86a56fc7e9c68940e185b6c1ebff529d8e
Georg <georg.pirklbauer@dynatrace.com>
2021-02-18 18:26:18 +0100

    update OneAgentMetadataEnricher and add tests

--- dynatrace/src/main/java/com/dynatrace/opentelemetry/metric/OneAgentMetadataEnricher.java
+++ dynatrace/src/main/java/com/dynatrace/opentelemetry/metric/OneAgentMetadataEnricher.java
@@ -1,24 +1,41 @@
+/*
+ * Copyright 2020 Dynatrace LLC
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dynatrace.opentelemetry.metric;
+
+import com.google.common.base.Strings;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.google.common.base.Strings;
-
 public class OneAgentMetadataEnricher {
     private final Logger logger;
 
     public OneAgentMetadataEnricher(Logger logger) {
         this.logger = logger;
+    }
+
+    public Collection<AbstractMap.SimpleEntry<String, String>> getDimensionsFromOneAgentMetadata() {
+        return parseOneAgentMetadata(getMetadataFileContent());
     }
 
     /**
@@ -31,7 +48,7 @@ public class OneAgentMetadataEnricher {
      * separated by the first occurring equal sign on each line, respectively. If no line is parsable, returns an empty
      * list
      */
-    public Collection<AbstractMap.SimpleEntry<String, String>> parseOneAgentMetadata(Collection<String> lines) {
+    protected Collection<AbstractMap.SimpleEntry<String, String>> parseOneAgentMetadata(Collection<String> lines) {
         ArrayList<AbstractMap.SimpleEntry<String, String>> entries = new ArrayList<>();
 
         // iterate all lines from OneAgent metadata file.
@@ -85,10 +102,9 @@ public class OneAgentMetadataEnricher {
         if (!Strings.isNullOrEmpty(secretFileName)) {
             List<String> properties;
             // read all lines in the secret file into the properties list:
-            try (Stream<String> lines = Files.lines(Paths.get(secretFileName))){
+            try (Stream<String> lines = Files.lines(Paths.get(secretFileName))) {
                 properties = lines.collect(Collectors.toList());
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 // in this case we can probably raise a warning, as the magic file has been found, but not the
                 // properties file pointed to by the magic file.
                 logger.warning(String.format("OneAgent properties file could not be read: %s", e.getMessage()));
